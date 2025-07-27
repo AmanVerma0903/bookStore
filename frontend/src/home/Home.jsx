873cc984ce110e1762f23d458eb3c68cd8de6843
AmanVerma0903 <vermaman1921@gmail.com>
2025-07-27 07:26:54 +0530

    Fix Footer import case sensitivity

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner'
 import FreeBook from '../components/FreeBook'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 export default function Home() {
    return (
